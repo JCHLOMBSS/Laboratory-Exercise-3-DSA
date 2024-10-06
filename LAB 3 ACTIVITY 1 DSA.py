509c4c07216130e5bd1b061dd7b3c7d37ae83e08
Joseph Chryswyn M. Lomboy <151927435+JCHLOMBSS@users.noreply.github.com>
2024-10-06 18:41:50 +0800

    Update LAB 3 ACTIVITY 1 DSA.py

--- LAB 3 ACTIVITY 1 DSA.py
+++ LAB 3 ACTIVITY 1 DSA.py
@@ -1,11 +1,9 @@
 def get_squared_odds(input_list):
     return [x**2 for x in input_list if x % 2 != 0]
 
-sample_list1 = [2, 4, 3]
-sample_list2 = [0, 0, 1, 1]
+input_str = input("Please enter your numbers then separate it with spaces: ")
+input_list = [int(x) for x in input_str.split()]
 
-squared_odds1 = get_squared_odds(sample_list1)
-squared_odds2 = get_squared_odds(sample_list2)
+squared_odds = get_squared_odds(input_list)
 
-print(f"Squared odds from {sample_list1} {squared_odds1}")
-print(f"Squared odds from {sample_list2} {squared_odds2}")
+print(f"Squared odds from {input_list}: {squared_odds}")
