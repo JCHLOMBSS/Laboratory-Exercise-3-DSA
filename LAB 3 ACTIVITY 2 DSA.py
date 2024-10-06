420cd6f6b6a402f7bd7b248abf849d19bcb51b2a
Joseph Chryswyn M. Lomboy <151927435+JCHLOMBSS@users.noreply.github.com>
2024-10-06 18:42:55 +0800

    Update LAB 3 ACTIVITY 2 DSA.py

--- LAB 3 ACTIVITY 2 DSA.py
+++ LAB 3 ACTIVITY 2 DSA.py
@@ -1,8 +1,13 @@
 def create_tuples(list1, list2):
-    return [(i+1, list2[i]) for i in range(len(list2))]
+    return [(list1[i], list2[i]) for i in range(len(list2))]
 
-list1 = [1, 2, 3]
-list2 = ["mark", "alice", "john"]
+input_str1 = input("Please Enter the list of numbers separated by spaces: ")
+list1 = [int(x) for x in input_str1.split()]
+list2 = []
+
+for i in range(len(list1)):
+    name = input(f"Please Enter a name for the number {list1[i]}: ")
+    list2.append(name)
 
 result = create_tuples(list1, list2)
 print(f"Result: {result}")
