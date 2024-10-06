d452b80f7c561d6b699a5bd80e809393e7c2f9ea
Joseph Chryswyn M. Lomboy <151927435+JCHLOMBSS@users.noreply.github.com>
2024-10-06 18:44:14 +0800

    Update LAB 3 ACTIVITY 3 DSA.py

--- LAB 3 ACTIVITY 3 DSA.py
+++ LAB 3 ACTIVITY 3 DSA.py
@@ -2,6 +2,8 @@ def vowelsToUpper(s):
     vowels = 'aeiou'
     return ''.join([char.upper() if char in vowels else char for char in s])
 
-print(vowelsToUpper(""))
-print(vowelsToUpper("Hello, world!"))
-print(vowelsToUpper("hello hi bye"))
+while True:
+    sentence = input("Enter a short word or sentence (or type 'no' to stop): ")
+    if sentence.lower() == 'no':
+        break
+    print(vowelsToUpper(sentence))
